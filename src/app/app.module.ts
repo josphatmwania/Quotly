2ee84f491b5c0300c096d44876687a6bac28d35b
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-07-19 00:07:12 +0300

    Add Quote, Form & Quote detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
