8729419faba0209c6b9a8e11504f6a637c5c4281
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-07-19 02:22:16 +0300

    Fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { DateCountPipe } from './date-count.pipe';
+// import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+// import { QuoteFormComponent } from './quote-form/quote-form.component';
+// import { DateCountPipe } from './date-count.pipe';
 import {FormsModule }  from  '@angular/forms';
-import { HighlightersComponent } from './highlighters/highlighters.component';
-import { HighlightersDirective } from './highlighters.directive';
+// import { HighlightersComponent } from './highlighters/highlighters.component';
+import { HighlightersDirective } from './quote-detail/highlighters.directive';
 
 
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quote';
+import { Quote } from '../quote-detail/quote';
 
 @Component({
   selector: 'app-quote',
@@ -10,28 +10,26 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[ ] =[
-      new Quote('Productivity is being able to do things that you were never able to do before' 'Franz Kafka', 'Me',  new Date(2021, 7, 18), 0,0),
+      new Quote('Productivity is being able to do things that you were never able to do before' ,'Franz Kafka', 'Me',  new Date(2021, 7, 18), 0,0),
       new Quote('Dont be a slave to your emotion.Control them','Buddha', 'Me',  new Date(2021, 7, 17),0,0)
   ];
 
 
 // Adding new quotes 
 
-  addNewQuote(quote){
+  addNewQuote(quote:Quote){
     let quoteLength = this.quotes.length;
-    quote.id = quoteLength + 1;
-    quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
   
-  toggleDetails(index){
+  toggleDetails(index:number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
     
   }
   
 // Deleting a quote 
 
-  deleteQuote(isComplete, index){
+  deleteQuote(isComplete:boolean, index:number){
     if (isComplete){
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
 
