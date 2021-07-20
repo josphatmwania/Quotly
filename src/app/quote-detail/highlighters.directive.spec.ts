e9c165d266fe55c89f7b61c34fe25a53be9ae0f4
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-07-20 23:17:06 +0300

    Change directory

--- src/app/quote-detail/highlighters.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightersDirective } from './highlighters.directive';
-
-describe('HighlightersDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightersDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/quote-detail/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-    public showDescription: boolean;
-    constructor(public quote: string,public author: string,public submittedBy: string ,public since: Date, public upVote: number, public downVote :number){
-      this.showDescription=false;
-    }
-
-
-}
